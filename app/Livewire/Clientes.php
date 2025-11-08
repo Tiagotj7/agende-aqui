453fefc7c499e80ef4d7d325f49250bdb71c0032
Tiagotj7 <tiagowork07@gmail.com>
2025-11-08 01:00:35 -0300

    adjust

--- app/Livewire/Clientes.php
+++ app/Livewire/Clientes.php
@@ -2,17 +2,27 @@
 
 namespace App\Livewire;
 
-use Livewire\Component;
 use App\Models\Cliente;
+use Livewire\Component;
 
 class Clientes extends Component
 {
-    public $clientes, $nome, $email, $telefone, $cliente_id;
+    public $clientes;
+
+    public $nome;
+
+    public $email;
+
+    public $telefone;
+
+    public $cliente_id;
+
     public $isEdit = false;
 
     public function render()
     {
         $this->clientes = Cliente::all();
+
         return view('livewire.clientes');
     }
 
