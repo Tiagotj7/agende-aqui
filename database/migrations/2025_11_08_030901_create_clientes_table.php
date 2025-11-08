62f78dd458d66f2b2a5b9d5721cf0f38b2835c29
Tiagotj7 <tiagowork07@gmail.com>
2025-11-08 01:20:13 -0300

    edit tests

--- database/migrations/2025_11_08_030901_create_clientes_table.php
+++ database/migrations/2025_11_08_030901_create_clientes_table.php
@@ -1,10 +1,30 @@
-public function up(): void
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::create('clientes', function (Blueprint $table) {
-        $table->id();
-        $table->string('nome');
-        $table->string('email')->unique();
-        $table->string('telefone')->nullable();
-        $table->timestamps();
-    });
-}
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('telefone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
